958fee46a05819f5f58a728cb577dfc38f94d801
WatersTimms <abolajiemmanuel774@gmail.com>
2022-03-14 20:23:54 -0700

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+* main - entry point
+* print the lowercase of an alphabet
+* Return: 0
+*/
+int main(void)
+{
+int c = 0;
+while (c < 10)
+{
+	printf("%d\n", c);
+	c++;
+}
+/* your code goes there */
+return (0);
+}
