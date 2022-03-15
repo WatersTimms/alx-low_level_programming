aefb7e1f372a60a43d69caefa3f942ee964c15b3
WatersTimms <abolajiemmanuel774@gmail.com>
2022-03-14 17:35:46 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+* main - entry point
+* print the lowercase of an alphabet
+* Return: 0
+*/
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+	putchar(c);
+	c++;
+}
+	putchar('\n');
+/* your code goes there */
+return (0);
+}
